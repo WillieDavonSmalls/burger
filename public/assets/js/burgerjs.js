448f066636469570456f8ae9f16da59f375a08f6
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-07-25 21:48:25 -0400

    reorganized the JS functions & HTML

--- public/assets/js/burgerjs.js
+++ public/assets/js/burgerjs.js
@@ -24,7 +24,7 @@ function loadDevouredBurgers(){
         html += '</table>';
         return html;
     }
-
+//AJAX GET for devoured burgers
 jQuery.ajax({
     method: 'GET',
     url: '/api/burger_controller',
@@ -42,8 +42,9 @@ jQuery.ajax({
 
 //Function for building Available Burgers
 function buildAvailableBurgersForm(){
+
     function fillInAvailableBurgerInfo(data){
-        var html;
+        var html = `<div id="availBurgers">`;
         data.forEach(function(data) {
             html += `
                 <form class="form-inline" id="eatMe">
@@ -53,17 +54,19 @@ function buildAvailableBurgersForm(){
                     <button type="button" class="btn btn-success btn-xs" id="btnRemoveForm">Eat it Up!</button>
                 </form>`
             });
+            html += `</div>`
             return html;
     };
 
-    
+    //AJAX GET for available burgers
     jQuery.ajax({
         method: 'GET',
         url: '/api/available',
         dataType: 'json',
         success: function(data){
             console.log(data);
-            $("#availBurgers").after(fillInAvailableBurgerInfo(data));
+            $("#availBurgers").remove();
+            $("#availBurgers_h4").after(fillInAvailableBurgerInfo(data));
         },
         error: function(e){
             console.error(e)
@@ -72,36 +75,31 @@ function buildAvailableBurgersForm(){
 };
 
 
-jQuery(document).ready(function() {
-    var form = jQuery('#form');
-    form.find('button').on('click', function() {
-        var burgerType = form.find('textarea').val();
-        var payload = {new_burgertype : burgerType};
 
-        console.log('about to send post request', payload);
+$(document).on('click', '#insertNewBurger', function(){
+    var burgerType = $("textarea[name='burger']").val();
+    var payload = {new_burgertype : burgerType};
+    console.log('about to send post request', payload);
 
-        jQuery.ajax({
-            type: 'POST',
-            url: '/api/new_burger',
-            data: JSON.stringify(payload),//turn data into JSON string
-            contentType: "application/json; charset=utf-8",
-            success: function() {
-                console.log('sending post request', payload);
-                buildAvailableBurgersForm();
-            },
-            error: function(e) {
-                console.error(e);
-            }
-        }); 
-
-    });
-
+    jQuery.ajax({
+        type: 'POST',
+        url: '/api/new_burger',
+        data: JSON.stringify(payload),//turn data into JSON string
+        contentType: "application/json; charset=utf-8",
+        success: function() {
+            // console.log('sending post request', payload);
+            buildAvailableBurgersForm();
+        },
+        error: function(e) {
+            console.error(e);
+        }
+    }); 
 });
 
 //Function that Devours the Burgers and Puts them into the Devoured List
 $(document).on('click', '#btnRemoveForm', function(){
     var SQLid = $(this).closest("form.form-inline").find("input[name='burger']").val();
-    alert(SQLid);
+    // alert(SQLid);
     $(this).closest("form.form-inline").remove();
     var payload = {burgerSQLID : SQLid};
 
